25335962c129eb6d6ea280f382fcc9c99ef5aefb
Sumit Saini <sumitsaini199711@gmail.com>
2021-04-15 22:35:17 +0530

    Setting up postgres db with nodejs

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 app.use(cors());
 app.use(express.json());
+
+//ROUTES//
+
+//Create a ToDo
+
+//Get all todo
+
+//get a Todo
+
+//update a todo
+
+//delete a todo
 
 app.listen(8080, () => {
   console.log("Listining on 8080");
